9f65fefec580da5ccd5c08234d9f03a34aca3604
bsfeng <bsfeng@foxmail.com>
2020-08-13 19:42:12 +0800

    (+)配置自定义轮询

--- cloud-consumer-order/src/main/java/com/example/cloud/order/OrderApplication.java
+++ cloud-consumer-order/src/main/java/com/example/cloud/order/OrderApplication.java
@@ -1,15 +1,19 @@
 package com.example.cloud.order;
 
+import com.example.cloud.rule.MyRoundRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 //通过开启此服务，便可以实现微服务注册
 @EnableEurekaClient
 //通过开启此服务，就可以通过discoveryClient获得远程微服务信息
 @EnableDiscoveryClient
+//配置对指定的微服务使用自定义的规则
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyRoundRule.class)
 public class OrderApplication {
 
 	public static void main(String[] args) {
