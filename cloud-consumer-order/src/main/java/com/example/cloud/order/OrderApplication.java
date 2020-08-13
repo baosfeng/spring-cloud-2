8d6f9f6a45a9ff3d012841af5735f334e64c660f
bsfeng <bsfeng@foxmail.com>
2020-08-13 17:52:32 +0800

    (+)提供服务发现

--- cloud-consumer-order/src/main/java/com/example/cloud/order/OrderApplication.java
+++ cloud-consumer-order/src/main/java/com/example/cloud/order/OrderApplication.java
@@ -2,10 +2,14 @@ package com.example.cloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//通过开启此服务，便可以实现微服务注册
 @EnableEurekaClient
+//通过开启此服务，就可以通过discoveryClient获得远程微服务信息
+@EnableDiscoveryClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
--- cloud-consumer-order/src/main/java/com/example/cloud/order/web/OrderController.java
+++ cloud-consumer-order/src/main/java/com/example/cloud/order/web/OrderController.java
@@ -3,6 +3,8 @@ package com.example.cloud.order.web;
 import com.example.cloud.common.entity.Payment;
 import com.example.cloud.common.entity.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,6 +23,9 @@ public class OrderController {
 
 	//动态获取地址
 	public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
+	@Resource
+	private DiscoveryClient discoveryClient;
 
 	@Resource
 	private RestTemplate restTemplate;
@@ -31,5 +39,33 @@ public class OrderController {
 	@GetMapping("/select/{id}")
 	public Result getPayment(@PathVariable("id") Long id) {
 		return restTemplate.getForObject(PAYMENT_URL + "/payment/select/" + id, Result.class);
+	}
+
+	/**
+	 * 获得所有的注册微服务信息
+	 *
+	 * @return 所有的微服务注册信息
+	 */
+	@GetMapping("/instance")
+	public Result getInstance() {
+		List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+		Result result = new Result();
+		ArrayList<HashMap> list = new ArrayList<>();
+		instances.forEach(item -> {
+			HashMap<String, Object> map = new HashMap<>();
+			map.put("host", item.getHost());
+			map.put("port", item.getPort());
+			map.put("Metadata", item.getMetadata());
+			map.put("InstanceId", item.getInstanceId());
+			map.put("Scheme", item.getScheme());
+			map.put("ServiceId", item.getServiceId());
+			map.put("Uri", item.getUri());
+			list.add(map);
+		});
+		result.put("Instances", list);
+		List<String> services = discoveryClient.getServices();
+		result.put("services", services);
+
+		return result;
 	}
 }
