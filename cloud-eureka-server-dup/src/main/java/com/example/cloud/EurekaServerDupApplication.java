ce200fb8c6c4a74fc1a3fe8e4184176392309983
bsfeng <bsfeng@foxmail.com>
2020-08-13 16:51:20 +0800

    (+)注册Eureka集群

--- cloud-eureka-server-dup/src/main/java/com/example/cloud/EurekaServerDupApplication.java
+++ cloud-eureka-server-dup/src/main/java/com/example/cloud/EurekaServerDupApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer {
+public class EurekaServerDupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServer.class, args);
+		SpringApplication.run(EurekaServerDupApplication.class, args);
 	}
 }
