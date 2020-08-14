482bd8bf42931a313f37f61b378a0fe0d1b1bcb3
bsfeng <bsfeng@foxmail.com>
2020-08-14 08:11:06 +0800

    (+)配置服务端的熔断

--- cloud-hystrix-provider-pay/src/main/java/com/example/cloud/HystrixApplication.java
+++ cloud-hystrix-provider-pay/src/main/java/com/example/cloud/HystrixApplication.java
@@ -2,10 +2,13 @@ package com.example.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//开启Hystrix断路器
+@EnableCircuitBreaker
 public class HystrixApplication {
 
 	public static void main(String[] args) {
--- cloud-hystrix-provider-pay/src/main/java/com/example/cloud/service/PaymentService.java
+++ cloud-hystrix-provider-pay/src/main/java/com/example/cloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.example.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -13,6 +15,10 @@ public class PaymentService {
 	}
 
 	//失败
+	@HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler", commandProperties = {
+			//2秒钟以内就是正常的业务逻辑
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+	})
 	public String paymentInfoTimeOut(Integer id) {
 		int timeNumber = 3;
 		try {
@@ -21,6 +27,10 @@ public class PaymentService {
 			e.printStackTrace();
 		}
 		return "线程池：" + Thread.currentThread().getName() + "   paymentInfoTimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" + timeNumber;
+	}
+
+	public String paymentInfoTimeOutHandler(Integer id) {
+		return "系统繁忙，当前处理的id为：" + id;
 	}
 
 }
